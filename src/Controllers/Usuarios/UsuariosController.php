07abd738ce941fb0ceb199309312d68e670e516a
antoniosaints <costaantonio883@gmail.com>
2024-04-08 13:05:02 -0300

    alteracoes

--- src/Controllers/Usuarios/UsuariosController.php
+++ src/Controllers/Usuarios/UsuariosController.php
@@ -3,27 +3,28 @@ namespace App\Controllers\Usuarios;
 
 use App\Controllers\BaseController;
 use App\Http\ErrorHandler;
+use App\Http\Request;
+use App\Http\Response;
 use App\Models\UsuarioModel;
 use App\Schemas\UsuariosSchema;
-use App\Validators\DatabaseValidator;
 use Exception;
 
 class UsuariosController extends BaseController
 {
-    public function Create($request, $response)
+    public function Create(Request $request, Response $response)
     {
         try {
-            $Model = new UsuarioModel();
-            $body = $request::getJson();
-            $data = DatabaseValidator::validate(UsuariosSchema::createUser(), $body);
-            $user = $Model->save($data);
-            $response::json([
+            $Model = new UsuarioModel(); // Instanciando o Model
+            $body = $request::getJson(); // Recebe os dados do JSON enviados
+            $data = self::validateSchema(UsuariosSchema::createUser(), $body); // Valida os dados do JSON
+            $user = $Model->save($data); // Salva os dados no Model
+            $response::json([ // Retorna os dados no formato JSON
                 'message' => 'Usuário criado com sucesso',
                 'id'      => $user,
                 'status'  => 200
             ], 200);
-        }catch (Exception $e) {
-            ErrorHandler::handle($e);
+        }catch (Exception $e) { // Tratamento de erro
+            ErrorHandler::handle($e); // Chama o tratamento de erro
         }
     }
 }
