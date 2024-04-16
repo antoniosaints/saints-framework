a97f48088ced783773440e80b4d29a8bd4763c56
antoniosaints <costaantonio883@gmail.com>
2024-04-16 08:49:51 -0300

    added support to view render and cors solve

--- src/Core/Http/Response.php
+++ src/Core/Http/Response.php
@@ -1,5 +1,5 @@
 <?php
-namespace App\Core\Http;
+namespace App\Http;
 
 class Response 
 {
@@ -14,4 +14,37 @@ class Response
         }
         echo json_encode($data);
     }
+
+    public static function html(string $html, int $code = 200)
+    {
+        http_response_code($code);
+        header('Content-Type: text/html');
+        header('Access-Control-Allow-Origin: *');
+        header('Powered-By: saints-framework');
+        echo $html;
+    }
+
+    public static function view(string $view, array $data = [], int $code = 200)
+    {
+        http_response_code($code);
+        header('Content-Type: text/html');
+        header('Access-Control-Allow-Origin: *');
+        header('Powered-By: saints-framework');
+        
+        // Caminho completo para o arquivo de visualização
+        $viewPath = __DIR__ . '/../../Views/' . $view . '.php';
+        
+        // Verifica se o arquivo de visualização existe
+        if (file_exists($viewPath)) {
+            // Extrai os dados para torná-los acessíveis na visualização
+            extract($data);
+            
+            // Inclui o arquivo de visualização
+            include $viewPath;
+        } else {
+            // Se o arquivo de visualização não existir, retorna uma mensagem de erro
+            echo "Erro: O arquivo de visualização '$view.php' não foi encontrado.";
+        }
+    }
+    
 }
