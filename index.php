9228219d1b01471d1b77664ff53105bda44a0ce4
Antonio Santos <68619881+antoniosaints@users.noreply.github.com>
2024-05-04 20:31:53 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/src/Core/Bootstrap.php';
+require_once __DIR__ . '/src/Core/Http/HeaderCheck.php';
+require_once __DIR__ . '/src/Core/Helper/helper_core.php';
+
+foreach (glob(__DIR__ . '/src/Routes/*.php') as $filename) {
+    require_once $filename;
+}
 
 use App\Core\Core;
 use App\Core\Http\Route;
